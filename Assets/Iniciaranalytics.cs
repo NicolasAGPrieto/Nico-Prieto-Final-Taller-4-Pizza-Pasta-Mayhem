ab1e9be8e7f5d3c446aa264898434ff51c98ebbd
NicolasAGPrieto <67789623+NicolasAGPrieto@users.noreply.github.com>
2024-02-25 21:21:58 -0300

    Add files via upload

---
+++ Assets/Iniciaranalytics.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using Unity.Services.Core;
+using Unity.Services.Analytics;
+using System.Collections.Generic;
+
+public class Iniciaranalytics : MonoBehaviour
+{
+    public static Iniciaranalytics instancia;
+
+/*
+    private void Awake()
+    {
+
+        if(instancia!=null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instancia = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+*/
+
+
+    async void Start()
+    {
+        await UnityServices.InitializeAsync();
+
+        ConsentGiven();
+    }
+
+    void AskForConsent()
+    {
+        // ... show the player a UI element that asks for consent.
+    }
+
+    void ConsentGiven()
+    {
+        AnalyticsService.Instance.StartDataCollection();
+    }
+}
+
